8d869b6fac909762ca711f3a9d7fde08558e55b1
kezzyj <karachidavid31@gmail.com>
2023-02-14 08:55:31 -0800

    checking pretty syntax

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -7,15 +7,10 @@ list all documents in a collection
 """
 
 
-
-
-
 def list_all(mongo_collection):
 
     """
-
     return an empty list if no document in the collection
-
     """
 
     if mongo_collection.count() == 0:
@@ -24,4 +19,4 @@ def list_all(mongo_collection):
 
     else:
 
-        return mongo_collection.find().pretty()
+        return mongo_collection.find()
